e55b4968fd314bda3c0f4acbe5eb5a33d99447a8
Michał Pasternak <michal.dtz@gmail.com>
2020-08-08 23:46:50 +0200

    Create superuser

--- stable/containers/bpp/entrypoint-appserver.sh
+++ stable/containers/bpp/entrypoint-appserver.sh
@@ -12,6 +12,8 @@ fi
 
 bpp-manage.py migrate
 
+bpp-manage.py shell -c "from bpp.models import BppUser; BppUser.objects.create_superuser('admin', 'admin@example.com', 'admin')" || true
+
 export DJANGO_SETTINGS_MODULE=django_bpp.settings.production
 
 exec uvicorn --host 0.0.0.0 --port=8080 django_bpp.asgi:application
