1c58053cf556efc058397faec4b024796c5e2979
Michał Pasternak <michal.dtz@gmail.com>
2020-08-08 23:35:28 +0200

    Fix all

--- stable/containers/bpp/entrypoint-appserver.sh
+++ stable/containers/bpp/entrypoint-appserver.sh
@@ -2,14 +2,16 @@
 
 createdb bpp || true
 
-staticroot=/usr/local/lib/python3.7/site-packages/django_bpp/staticroot
+staticroot=/usr/local/lib/python3.8/site-packages/django_bpp/staticroot
 
 if [ ! -L $staticroot ]; then
-    cp -R /usr/local/lib/python3.7/site-packages/django_bpp/staticroot/* /staticroot
+    cp -R /usr/local/lib/python3.8/site-packages/django_bpp/staticroot/* /staticroot
     rm -rf $staticroot
     ln -s /staticroot $staticroot
 fi    
 
 bpp-manage.py migrate
 
-exec uwsgi --module=django_bpp.wsgi --master --http-socket=:8080
+export DJANGO_SETTINGS_MODULE=django_bpp.settings.production
+
+exec uvicorn --host 0.0.0.0 --port=8080 django_bpp.asgi:application
