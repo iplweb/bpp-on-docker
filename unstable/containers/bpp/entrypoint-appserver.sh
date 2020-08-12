eaa75a18953a4b8d421d877b8ffcf7fcd7e790c2
Michał Pasternak <michal.dtz@gmail.com>
2020-08-12 18:39:00 +0200

    Fix unstable

--- unstable/containers/bpp/entrypoint-appserver.sh
+++ unstable/containers/bpp/entrypoint-appserver.sh
@@ -2,14 +2,18 @@
 
 createdb bpp || true
 
-staticroot=/usr/local/lib/python3.7/site-packages/django_bpp/staticroot
+staticroot=/usr/local/lib/python3.8/site-packages/django_bpp/staticroot
 
 if [ ! -L $staticroot ]; then
-    cp -R /usr/local/lib/python3.7/site-packages/django_bpp/staticroot/* /staticroot
+    cp -R /usr/local/lib/python3.8/site-packages/django_bpp/staticroot/* /staticroot
     rm -rf $staticroot
     ln -s /staticroot $staticroot
 fi    
 
 bpp-manage.py migrate
 
-exec uwsgi --module=django_bpp.wsgi --master --http-socket=:8080
+bpp-manage.py shell -c "from bpp.models import BppUser; BppUser.objects.create_superuser('admin', 'admin@example.com', 'admin')" || true
+
+export DJANGO_SETTINGS_MODULE=django_bpp.settings.production
+
+exec uvicorn --host 0.0.0.0 --port=8080 django_bpp.asgi:application
